df274cc2e3e2b343f0b05cca9220c7064ffff860
Gaspar Hermán <gaspar@GHLinux.home>
2022-03-07 19:31:40 +0100

    Initial Commit

---
+++ gateway.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -t nat -A PREROUTING -j LOG --log-prefix "iptables :"
+iptables -A FORWARD -m tos --tos 2 -j DROP
+iptables -A FORWARD -i eth0 -o eth1
+iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED
+
+IPTABLES_CONFIG_FOR_RSYSLOG_FILE="/etc/rsyslog.conf"
+echo '$template ipTraffic,"%$day%/%$month%/%$year% %timegenerated:12:19:date-rfc3339%, %msg%\n"' >> ${IPTABLES_CONFIG_FOR_RSYSLOG_FILE}
+echi "kern.*; /var/log/kern.log; ipTraffic" >> ${IPTABLES_CONFIG_FOR_RSYSLOG_FILE}
+
+/etc/init.d/rsyslog start
