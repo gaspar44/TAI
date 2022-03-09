a553d803fa092dbc3496b713f1f4839c91145b8f
David Mayordomo <57218796+Mayor25@users.noreply.github.com>
2022-03-09 10:38:13 +0100

    Update gateway.sh

--- gateway.sh
+++ gateway.sh
@@ -7,8 +7,8 @@ iptables -A FORWARD -m tos --tos 2 -j DROP
 iptables -A FORWARD -i eth0 -o eth1
 iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED
 
-# IPTABLES_CONFIG_FOR_RSYSLOG_FILE="/etc/rsyslog.conf"
-# echo '$template ipTraffic,"%$day%/%$month%/%$year% %timegenerated:12:19:date-rfc3339%, %msg%\n"' >> ${IPTABLES_CONFIG_FOR_RSYSLOG_FILE}
-# echo "kern.*; /var/log/kern.log; ipTraffic" >> ${IPTABLES_CONFIG_FOR_RSYSLOG_FILE}
+IPTABLES_CONFIG_FOR_RSYSLOG_FILE="/etc/rsyslog.conf"
+echo '$template ipTraffic,"%$day%/%$month%/%$year% %timegenerated:12:19:date-rfc3339%, %msg%\n"' >> ${IPTABLES_CONFIG_FOR_RSYSLOG_FILE}
+echo "kern.*; /var/log/kern.log; ipTraffic" >> ${IPTABLES_CONFIG_FOR_RSYSLOG_FILE}
 
 /etc/init.d/rsyslog start
