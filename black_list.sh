599cc286b3a99527b338530e685b720d7cf4b283
Gaspar Hermán <gaspar@gaspar-vit-p3400.home>
2022-03-10 18:38:24 +0100

    Changed IP range of affection

--- black_list.sh
+++ black_list.sh
@@ -4,4 +4,4 @@ iptables -N BLACK_LIST
 iptables -A BLACK_LIST -j LOG --log-prefix "Not allowed use to HTTP server "
 iptables -A BLACK_LIST -j DROP
 
-iptables -A INPUT -s 192.168.2.0/24 -p tcp --dport 80 -j BLACK_LIST
+iptables -A INPUT -s 192.168.1.0/24 -p tcp --dport 80 -j BLACK_LIST
